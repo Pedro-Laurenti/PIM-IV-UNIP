f49af21e61bc6e4bdcea9dd9e34f6a8fe2cece90
Pedro_Laurenti <pedrolaurenti04@gmail.com>
2023-04-01 10:26:31 -0300

    versão 1.3

--- Protótipo/Tentativa com HTML e CSS/script-valida-login.js
+++ Protótipo/Tentativa com HTML e CSS/script-valida-login.js
@@ -1,46 +1,28 @@
-// Obtém o formulário
 const form = document.querySelector('form');
 
-// Define uma função para validar o formulário
 function validarFormulario(event) {
-  // Previne o envio do formulário antes da validação
   event.preventDefault();
 
-  // Obtém os campos do formulário
   const usuario = form.querySelector('#usuario');
   const senha = form.querySelector('#senha');
 
-  // Verifica se os campos estão preenchidos
-  if (!usuario.value && !senha.value) {
-    exibirMensagemErro('Por favor, preencha os campos de usuário e senha.');
-    return;
-  } else if (!usuario.value) {
-    exibirMensagemErro('Por favor, preencha o campo de usuário.');
-    return;
-  } else if (!senha.value) {
-    exibirMensagemErro('Por favor, preencha o campo de senha.');
+  if (!usuario.value || !senha.value) {
+    exibirMensagemErro('Por favor, preencha todos os campos.');
     return;
   }
 
-  // Envia uma requisição AJAX para o script PHP
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'validar-login.php');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      if (xhr.responseText === 'OK') {
-        // Se a validação do PHP for bem-sucedida, envia o formulário
-        form.submit();
-      } else {
-        // Se a validação do PHP falhar, exibe uma mensagem de erro
-        exibirMensagemErro('Usuário ou senha inválidos.');
-      }
-    }
-  };
-  xhr.send(new FormData(form));
+  // Verifica se o usuário é válido
+  if (usuario.value === 'usuario1' && senha.value === 'senha1') {
+    // Redireciona para a página principal se o usuário for válido
+    window.location.href = 'pagina-principal.html';
+  } else if (usuario.value === 'usuario2' && senha.value === 'senha2') {
+    // Redireciona para a página de perfil se o usuário for válido
+    window.location.href = 'perfil.html';
+  } else {
+    exibirMensagemErro('Usuário ou senha inválidos.');
+  }
 }
 
-// Define uma função para exibir uma mensagem de erro
 function exibirMensagemErro(mensagem) {
   const form = document.querySelector('form');
   const errorMessage = document.createElement('div');
@@ -50,8 +32,7 @@ function exibirMensagemErro(mensagem) {
   errorMessage.classList.add('show');
   setTimeout(function() {
     errorMessage.classList.remove('show');
-  }, 20000);
+  }, 2000);
 }
 
-// Adiciona um evento de submit ao formulário
 form.addEventListener('submit', validarFormulario);
