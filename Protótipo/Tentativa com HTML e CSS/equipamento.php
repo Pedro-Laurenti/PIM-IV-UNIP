5dab9ae1da1bc39fc48ff9c47277d0fcb6d7c469
Pedro_Laurenti <pedrolaurenti04@gmail.com>
2023-04-08 00:15:48 -0300

    versão 2.2 (tela de equipamento implementada)

--- Protótipo/Tentativa com HTML e CSS/equipamento.php
+++ Protótipo/Tentativa com HTML e CSS/equipamento.php
@@ -11,12 +11,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://code.jquery.com/jquery-3.6.1.js"></script>
     <script src="https://kit.fontawesome.com/09b3ced86b.js" crossorigin="anonymous"></script>
-	<title>StockEdu - Início</title>
+	<title>StockEdu - Equipamentos</title>
 </head>
 <body>
 	<header id="header">
         <div class="centro">
-            <a href="">
+            <a href="main.php">
                 <img src="img/5 - logo-deitada.png" class="logo-hor" title="Logo StockEdu">
             </a>
         </div>
@@ -24,48 +24,36 @@
             <button id="btn-mobile"><i class="fa-solid fa-bars"></i>
             </button>
             <ul id="menu" role="menu">
-                <li><a href="#wrapper-galer-prod">Equipamentos</a></li>
-                <li><a href="#minhas-requisicoes">Minhas aquisições</a></li>
+                <li><a href="main.php">Equipamentos</a></li>
+                <li><a href="main.php">Minhas aquisições</a></li>
             </ul>
         </nav>
     </header>
 	<div id="wrapper">
         <main>
-            <div id="wrapper-slide">
-                <!-- Slideshow container -->
-                <div class="slideshow-container">
-                    <!-- Full-width images with number and caption text -->
-                    <div class="mySlides fade">
-                        <div class="numbertext">1 / 4</div>
-                        <img src="img/6---slide-1.png" style="width:100%">
-                        <a class="btnsld" href="https://www.google.com">Acesse aqui</a>
+            <div id="wrapper-galer-prod">
+                <div class="product-inside">
+                    <img src="img\8 - produto-2.png" alt="Notebook">
+                    <div class="description">
+                    <h2>Notebook ABC123</h2>
+                    <p>Este é um notebook com processador Intel Core i5, 8GB de RAM e 512GB de armazenamento SSD.</p>
+                    <h3>Especificações técnicas:</h3>
+                    <ul>
+                        <li>Processador: Intel Core i5</li>
+                        <li>Memória RAM: 8GB DDR4</li>
+                        <li>Armazenamento: 512GB SSD</li>
+                        <li>Tela: 14 polegadas</li>
+                        <li>Resolução da tela: Full HD (1920 x 1080)</li>
+                        <li>Placa de vídeo: Integrada Intel UHD Graphics</li>
+                        <li>Sistema Operacional: Windows 10</li>
+                        <li>Conectividade: Wi-Fi, Bluetooth, 3x USB, 1x HDMI, 1x RJ45, 1x entrada para fone de ouvido</li>
+                        <li>Bateria: 3 células, 45Wh</li>
+                        <li>Dimensões: 32,5 x 22,7 x 1,8 cm</li>
+                        <li>Peso: 1,5 kg</li>
+                    </ul>
                     </div>
-                    <div class="mySlides fade">
-                        <div class="numbertext">2 / 4</div>
-                        <img src="img/6---slide-2.png" style="width:100%">
-                        <a class="btnsld" href="https://www.google.com">Acesse aqui</a>
-                    </div>
-                    <div class="mySlides fade">
-                        <div class="numbertext">3 / 4</div>
-                        <img src="img/6---slide-3.png" style="width:100%">
-                        <a class="btnsld" href="https://www.google.com">Acesse aqui</a>
-                    </div>
-                    <div class="mySlides fade">
-                        <div class="numbertext">4 / 4</div>
-                        <img src="img/6---slide-4.png" style="width:100%">
-                        <a class="btnsld" href="https://www.google.com">Acesse aqui</a>
-                    </div>
-                    <!-- Next and previous buttons -->
-                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                </div>
-                <br>
-                <!-- The dots/circles -->
-                <div style="text-align:center">
-                    <span class="dot" onclick="currentSlide(1)"></span>
-                    <span class="dot" onclick="currentSlide(2)"></span>
-                    <span class="dot" onclick="currentSlide(3)"></span>
-                    <span class="dot" onclick="currentSlide(4)"></span>
+                    <br>
+                    <button class="add-to-cart">Adicionar ao carrinho</button>
                 </div>
             </div>
             <div id="wrapper-galer-prod">
@@ -74,7 +62,7 @@
                 <br>
                 <div class="product-grid">
                     <div class="product">
-                        <a href="equipamento.php" class="prod-cont">
+                        <a href="#" class="prod-cont">
                         <img src="img\7 - produto-1.png" alt="Produto 1">
                         <div class="product-details">
                             <h3>Datashow ProjeX</h3>
@@ -86,19 +74,19 @@
                         </a>
                     </div>
                     <div class="product">
-                        <a href="equipamento.php" class="prod-cont">
-                            <img src="img\8 - produto-2.png" alt="Produto 2">
-                                <div class="product-details">
-                                <h3>Notebook InfinitoBook 5</h3>
-                                <li>Processador: Intel Core i7</li>
-                                <li>Armazenamento: SSD de 512 GB</li>
-                                <li>Placa de vídeo: NVIDIA GeForce MX350</li>
-                                <u id="dash-do-prod">ver mais...</u>
-                            </div>
+                        <a href="#" class="prod-cont">
+                        <img src="img\8 - produto-2.png" alt="Produto 2">
+                        <div class="product-details">
+                            <h3>Notebook InfinitoBook 5</h3>
+                            <li>Processador: Intel Core i7</li>
+                            <li>Armazenamento: SSD de 512 GB</li>
+                            <li>Placa de vídeo: NVIDIA GeForce MX350</li>
+                            <u id="dash-do-prod">ver mais...</u>
+                        </div>
                         </a>
                     </div>
                     <div class="product">
-                        <a href="equipamento.php" class="prod-cont">
+                        <a href="#" class="prod-cont">
                             <img src="img\9 - produto-3.png" alt="Produto 3">
                             <div class="product-details">
                                 <h3>Caixa de som BoomPod</h3>
